be66af6879bb3529784d934d15adb83815f637ca
Ceyhun-gl <ceyhungul148@gmail.com>
2023-11-15 22:04:21 +0300

    first commit

---
+++ src/config/index.js
@@ -0,0 +1,15 @@
+const Config = {
+    components: {
+      icon: {
+        width: 15,
+        height: 15,
+      },
+    },
+    app: {
+      name: "E-Commerce",
+    },
+    lang: {
+      default: "en",
+    },
+  };
+  export default Config;
