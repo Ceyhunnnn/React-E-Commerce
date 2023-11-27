88a3421bd180d818faa7991308aafefa8102873c
Ceyhun-gl <ceyhungul148@gmail.com>
2023-11-27 10:46:14 +0300

    category page finished

--- src/pages/CategoryProductPage/CategoryProductPage.js
+++ src/pages/CategoryProductPage/CategoryProductPage.js
@@ -1,10 +1,56 @@
 import React from "react";
 import "./CategoryProductPage.css";
 import { useLocation } from "react-router-dom";
+import Container from "components/Container";
+import { Button, Select } from "antd";
+import ShoppingCard from "components/ShoppingCard";
+
 function CategoryProductPage() {
   const path = useLocation();
-
-  return <div>{JSON.stringify(path.pathname.split("/")[2])}</div>;
+  console.log(JSON.stringify(path.pathname.split("/")[2]));
+  const orderSelectList = [
+    {
+      value: "top seller",
+      label: "top seller",
+    },
+    {
+      value: "from cheap to expensive",
+      label: "from cheap to expensive",
+    },
+    {
+      value: "from expensive to cheap",
+      label: "from expensive to cheap",
+    },
+  ];
+  const title = path.pathname.split("/")[2];
+  return (
+    <Container>
+      <div className="category-title">
+        <h1>{title[0].toUpperCase() + title.slice(1)}</h1>
+        <Select
+          defaultValue="top-seller"
+          style={{
+            width: 150,
+          }}
+          options={orderSelectList}
+        />
+      </div>
+      <div className="shopping-area">
+        <ShoppingCard />
+        <ShoppingCard />
+        <ShoppingCard />
+        <ShoppingCard />
+        <ShoppingCard />
+        <ShoppingCard />
+        <ShoppingCard />
+        <ShoppingCard />
+        <ShoppingCard />
+      </div>
+      <div className="flex-area" style={{ margin: "30px 0px" }}>
+        <Button>More</Button>
+      </div>
+    </Container>
+  );
 }
 
 export default CategoryProductPage;
