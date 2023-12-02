fc46ddd5c9eb5319a799c3eac54fde52c93d30f1
Ceyhun-gl <ceyhungul148@gmail.com>
2023-12-02 22:35:33 +0300

    edit for netlify

--- src/pages/AboutPage/AboutPage.js
+++ src/pages/AboutPage/AboutPage.js
@@ -1,52 +1,51 @@
 import React from "react";
 import "./AboutPage.css";
 import { useTranslation } from "react-i18next";
-// import AboutCard from "./components/Card";
-// import AboutCard from "./../AboutPage/components/Card";
-// import ServiceCard from "./components/ServiceCard";
+import AboutCard from "./component/Card";
+import ServiceCard from "./component/ServiceCard";
 
 function About() {
   const { t } = useTranslation();
-  // const aboutPersonInfo = [
-  //   {
-  //     id: 0,
-  //     name: "Tom Cruise",
-  //     department: "Founder & Chairman",
-  //     imgPath: "/images/aboutPerson1.png",
-  //   },
-  //   {
-  //     id: 1,
-  //     name: "Emma Watson",
-  //     department: "Managing Director",
-  //     imgPath: "/images/aboutPerson2.png",
-  //   },
-  //   {
-  //     id: 2,
-  //     name: "Will Smith",
-  //     department: "Product Designer",
-  //     imgPath: "/images/aboutPerson3.png",
-  //   },
-  // ];
-  // const services = [
-  //   {
-  //     id: 0,
-  //     text: "FREE AND FAST DELIVERY",
-  //     desc: "Free delivery for all orders over $140",
-  //     img: "/images/services.png",
-  //   },
-  //   {
-  //     id: 1,
-  //     text: "24/7 CUSTOMER SERVICE",
-  //     desc: "Friendly 24/7 customer support",
-  //     img: "/images/servicesPhone.png",
-  //   },
-  //   {
-  //     id: 2,
-  //     text: "MONEY BACK GUARANTEE",
-  //     desc: "We reurn money within 30 days",
-  //     img: "/images/servicesSafe.png",
-  //   },
-  // ];
+  const aboutPersonInfo = [
+    {
+      id: 0,
+      name: "Tom Cruise",
+      department: "Founder & Chairman",
+      imgPath: "/images/aboutPerson1.png",
+    },
+    {
+      id: 1,
+      name: "Emma Watson",
+      department: "Managing Director",
+      imgPath: "/images/aboutPerson2.png",
+    },
+    {
+      id: 2,
+      name: "Will Smith",
+      department: "Product Designer",
+      imgPath: "/images/aboutPerson3.png",
+    },
+  ];
+  const services = [
+    {
+      id: 0,
+      text: "FREE AND FAST DELIVERY",
+      desc: "Free delivery for all orders over $140",
+      img: "/images/services.png",
+    },
+    {
+      id: 1,
+      text: "24/7 CUSTOMER SERVICE",
+      desc: "Friendly 24/7 customer support",
+      img: "/images/servicesPhone.png",
+    },
+    {
+      id: 2,
+      text: "MONEY BACK GUARANTEE",
+      desc: "We reurn money within 30 days",
+      img: "/images/servicesSafe.png",
+    },
+  ];
   return (
     <>
       <div className="about-area">
@@ -63,7 +62,7 @@ function About() {
           />
         </div>
       </div>
-      {/* <div className="about-card-area sized-box">
+      <div className="about-card-area sized-box">
         {aboutPersonInfo.map((person) => (
           <AboutCard
             key={person.id}
@@ -72,9 +71,9 @@ function About() {
             path={person.imgPath}
           />
         ))}
-      </div> */}
+      </div>
       <hr></hr>
-      {/* <div className="about-card-area sized-box">
+      <div className="about-card-area sized-box">
         {services.map((service) => (
           <ServiceCard
             key={service.id}
@@ -83,7 +82,7 @@ function About() {
             desc={service.desc}
           />
         ))}
-      </div> */}
+      </div>
     </>
   );
 }
